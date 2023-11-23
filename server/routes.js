966beb2e97f34e4eec04f90cfc1e96b5717faefc
jonathanj101 <jonatjimen@gmail.com>
2023-11-22 19:14:12 -0500

    user routes added

--- server/routes.js
+++ server/routes.js
@@ -1,21 +1,40 @@
 const routes = require("express").Router();
+const userController = require("./controllers/userController");
 // express validators
 const { validationResult } = require("express-validator");
 
-const addURLMiddleware = [
+// user routes
+
+const userRigesterMiddleware = [
     (req, res, next) => {
         console.log(validationResult(req));
         const error = validationResult(req);
-        console.log(error, error.array());
         if (!error.isEmpty()) {
             return res.status(422).json({
                 error: error.array()[0],
             });
         }
-        return next;
+        return next();
     },
+    userController.register,
 ];
 
-routes.get("/add-url", addURLMiddleware);
+routes.post("/user/register", userRigesterMiddleware);
+
+const userLoginMiddleware = [
+    (req, res, next) => {
+        console.log(validationResult(req));
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(422).json({
+                error: error.array()[0],
+            });
+        }
+        return next();
+    },
+    userController.login,
+];
+
+routes.post("/user/login", userLoginMiddleware);
 
 module.exports = routes;
