b5bce673f05ee5ad139f6c57ef566d0764327299
jonathanj101 <jonatjimen@gmail.com>
2023-11-22 19:12:04 -0500

    if db connection fails, then dont start server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./config/app");
-
-const db = require("./models/db.js");
-
-db();
 
 const port = +process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    console.log("server works!");
-    return res.status(200).json({
-        msg: "Hello world",
-    });
-});
+// app.get("/", (req, res) => {
+//     console.log("server works!");
+//     return res.status(200).json({
+//         msg: "Hello world",
+//     });
+// });
 
-const server = http.createServer(app);
-server.listen(8001, () => {
-    console.log("Express server working!");
+// if db connection fails, don't listen to upcoming requests. Don't start server
+mongoose.connection.once("open", () => {
+    const server = http.createServer(app);
+    server.listen(8001, () => {
+        console.log(`Server Listening on port ${port}`);
+    });
 });
