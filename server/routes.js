df24a37c9a601ff60385c257e3865c7a64c14824
jonathanj101 <jonatjimen@gmail.com>
2023-11-27 00:41:12 -0500

    added url middleware

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,40 @@
 const routes = require("express").Router();
 const userController = require("./controllers/userController");
+const urlController = require("./controllers/urlController");
 // express validators
 const { validationResult } = require("express-validator");
+
+// // url routes
+
+const addURLMiddleware = [
+    (req, res, next) => {
+        console.log(validationResult(req));
+        const error = validationResult(req);
+        console.log(error, error.array());
+        if (!error.isEmpty()) {
+            return res.status(422).json({
+                error: error.array()[0],
+            });
+        }
+        return next();
+    },
+    urlController.addURL,
+];
+
+routes.post("/url/add-url", addURLMiddleware);
+
+const deleteUrlsMiddleware = [
+    (req, res, next) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(422).json({
+                error: error.array()[0],
+            });
+        }
+        return next;
+    },
+    urlController.deleteUrls,
+];
 
 // user routes
 
