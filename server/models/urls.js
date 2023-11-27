47922736f4b9709d50e50292c6d7c9510a07bb4a
jonathanj101 <jonatjimen@gmail.com>
2023-11-27 00:39:50 -0500

    urls model created

---
+++ server/models/urls.js
@@ -0,0 +1,20 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const urlSchema = new Schema({
+    url: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    host: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+    shortUrl: {
+        type: Schema.Types.ObjectId,
+        ref: "short_urls",
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("urls", urlSchema);
