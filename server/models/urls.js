065a18c7e1e2a086f1a6bf49ad6607ad774e1b1a
jonathanj101 <jonatjimen@gmail.com>
2023-11-28 01:41:47 -0500

    added timestamp

--- server/models/urls.js
+++ server/models/urls.js
@@ -1,20 +1,27 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const urlSchema = new Schema({
-    url: {
-        type: Schema.Types.String,
-        required: true,
+const urlSchema = new Schema(
+    {
+        url: {
+            type: Schema.Types.String,
+            required: true,
+        },
+        host: {
+            type: Schema.Types.ObjectId,
+            ref: "users",
+            required: true,
+        },
+        shortUrl: {
+            type: Schema.Types.ObjectId,
+            ref: "short_urls",
+            required: true,
+        },
     },
-    host: {
-        type: Schema.Types.ObjectId,
-        ref: "users",
-        required: true,
-    },
-    shortUrl: {
-        type: Schema.Types.ObjectId,
-        ref: "short_urls",
-        required: true,
-    },
-});
+    {
+        timestamps: true,
+        getters: true,
+        versionKey: false,
+    }
+);
 
 module.exports = mongoose.model("urls", urlSchema);
