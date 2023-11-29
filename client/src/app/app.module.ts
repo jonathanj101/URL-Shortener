ad07e8c7e5a8b7cbcf4d0ded9b4634dbf2f7247e
jonathanj101 <jonatjimen@gmail.com>
2023-11-29 13:33:08 -0500

    imported both componentns add/table, material/ components as well

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from "@angular/material/form-field"
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatTableModule } from "@angular/material/table"
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UrlsTableComponent } from './views/urls-table/urls-table.component';
+import { AddUrlComponent } from './views/add-url/add-url.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlsTableComponent,
+    AddUrlComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
