f986c6012aba28c9d07e49a6b5f4b74950fbe011
jonathanj101 <jonatjimen@gmail.com>
2023-12-05 00:07:42 -0500

    added default-layout

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './container/default-layout/default-layout.component';
+import { AddUrlComponent } from './views/add-url/add-url.component';
+import { UrlsTableComponent } from './views/urls-table/urls-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: DefaultLayoutComponent,
+    data: {
+      title: "Home"
+    },
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
